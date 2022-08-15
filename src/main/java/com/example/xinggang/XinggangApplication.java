dbbd1dbde54fa745ea1cc4a0b6856966822bc726
wangzehua <relaxwang0714@gmail.com>
2022-08-15 16:59:18 +0800

    Initial commit

---
+++ src/main/java/com/example/xinggang/XinggangApplication.java
@@ -0,0 +1,13 @@
+package com.example.xinggang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinggangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinggangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xinggang/XinggangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xinggang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinggangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
